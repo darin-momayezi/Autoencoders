db3b05c7990a36633fdcb485d4ffef10a9b3ece9
darin-momayezi <77948633+darin-momayezi@users.noreply.github.com>
2022-06-26 17:53:32 -0400

    Update genericAutoencoder.py

--- genericAutoencoder.py
+++ genericAutoencoder.py
@@ -1,119 +1,92 @@
-'''A generic autoencoder architecture to start.'''
+'''Solving dfdt + c*dfdx = 0 on a spatio-temporal grid'''
 
-import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader, TensorDataset
-from sklearn import preprocessing
-import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy as np
+import pandas as pd
+from scipy.fftpack import fft, fft2, ifft2, fftn, ifftn, fftfreq
 
 
-##### Parameters #####
 
-input_size = 100
-hidden1 = int(input_size/10)
-latent_dim = 7
-epochs = 100
-lr = 0.001  # learning rate
-batch_size = 20
+##### PARAMETERS #####
 
+T = 50000  # max time
+X = 1000  # max x
+t_list = [100, 1000, 5000, 10000, 20000, 50000]  # Number of temporal grid points
+t = t_list[2]  # Temporal grid points
+x = int(X/10)  # Number of spatial grid points
+dt = T / t  # Size of time interval
+dx = X / x  # Size of spatial interval
 
-##### Data #####
+k = 0.001 * 1/dx  # low frequency (k << 1/dx)
+q = 1000 * 1/dx  # high frequency (q >> 1/dx)
+c = 10*np.pi
 
-df = pd.read_csv('/Users/darinmomayezi/Documents/Research/GrigorievLab/Autoencoder/1D_PDEs/1Dcoarsegrid.csv')
-dfT = df.T
-
-train_data = TensorDataset(torch.Tensor(preprocessing.normalize(df[:int(0.7*df.shape[0])])))
-eval_data = TensorDataset(torch.Tensor(preprocessing.normalize(df[int(0.7*df.shape[0]):])))
-train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=False)
-eval_loader = DataLoader(dataset=eval_data, batch_size=batch_size, shuffle=False)
+a = 1
+b = 0
 
 
-##### NN ######
 
-def autoencoder():
-    class Autoencoder(torch.nn.Module):
-        def __init__(self):
-            super().__init__()
-            self.encoder = nn.Sequential(nn.Linear(input_size, hidden1, bias=True, dtype=torch.float32),
-                                         nn.ReLU(),
-                                         nn.Linear(hidden1, latent_dim, bias=True, dtype=torch.float32))
-            
-            self.decoder = nn.Sequential(nn.Linear(latent_dim, hidden1, bias=True, dtype=torch.float32),
-                                         nn.ReLU(),
-                                         nn.Linear(hidden1, input_size, bias=True, dtype=torch.float32))
-            
-        def forward(self, x):
-            encoded = self.encoder(x)
-            decoded = self.decoder(encoded)
-            return decoded
-        
-    model = Autoencoder()
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0)
+##### DATA #####
+
+x_arr = np.linspace(0, X, x)  # Spatial data
+
+t_arr = np.linspace(0, T, t)  # Temporal data
+
+xx, tt = np.meshgrid(x_arr, t_arr)
+xx = np.round(xx, 0)
+tt = np.round(tt, 0)
+
+
+u = xx + c*tt
+
+F = a*np.cos(k*u) + b*np.cos(q*u)
+
+##### SAVE DATA #####
+
+np.save('/Users/darinmomayezi/Downloads/NSE2D[71]/1Dcoarsegrid.npy', F)
+
+pd.DataFrame(F.T).to_csv('/Users/darinmomayezi/Downloads/NSE2D[71]/1Dcoarsegrid.csv', header=None, index=None)
+
+
+
+# Fourier transform
+# fourier = fft2(F)
+# freq = fftfreq(x, dt)
+# for row in fourier:
+#     for idx, x in enumerate(row):
+#         if freq[idx] == 0:
+#             continue
+#         else:
+#             x = x / freq[idx]
+
+# F = ifft2(fourier)
+
+
+
+
+# Finite difference
+# F = np.zeros((n, m))  # Solution
+# for idt, t in enumerate(t_arr):
     
-    
-    train_loss = []
-    eval_loss = []
-    
-    def evaluate(model, eval_loader):
-        model.eval()
-        running_eval_loss = 0
+#     for idx, x in enumerate(x_arr):
         
-        for i, x in enumerate(eval_loader):
-            input_eval = torch.Tensor(x[0][0])
-            recone = model(input_eval)
-            losse = criterion(input_eval, recone)
-            running_eval_loss += losse.item()
-        eval_loss.append(running_eval_loss / len(eval_loader))
-        print(f"Evaluation Loss: {round(running_eval_loss / len(eval_loader), 20), latent_dim}")
+#         if idx != 0:
+#             F[idx, idt] = F[idx-1, idt-1] * ((1/dt + c/dx) / (-1/dt - c/dx))
+#         else:
+#             continue
 
-        
-    for epoch in range(epochs):
-        model.train()
-        running_train_loss = 0
-        print(f'Epoch: {epoch +1}')
-        
-        for i, y in enumerate(train_loader):
-            input_train = torch.Tensor(y[0][0])
-            recont = model(input_train)
-            losst = criterion(input_train, recont)
-            optimizer.zero_grad()
-            losst.backward()
-            optimizer.step()
-            running_train_loss += losst.item()
-        
-        for param_group in optimizer.param_groups:
-            print('lr: ', param_group['lr'])
-        train_loss.append(running_train_loss / len(train_loader))
-        evaluate(model, eval_loader)
-        print(f'Training Loss: {round(running_train_loss / len(train_loader), 20)}')
-    evalDict[m].append(eval_loss[-1])
-        
-   
-evalDict = {}    
-for i in range(1): 
-    for m in range(1, latent_dim+1):  # Dimension of manifold
+# Save solution
+# np.savetxt('/Users/darinmomayezi/Documents/Research/GrigorievLab/Autoencoder/1D_PDEs/1Dcoarsegrid.csv', fourier, delimiter=',')
 
-        latent_dim = m
-        
-        if latent_dim not in evalDict:
-            evalDict[latent_dim] = []
-            
-        autoencoder()
-    
 
-##### Plot results #####
-ranging = []
-errors = []
-for key, values in evalDict.items():
-    for value in values:
-        ranging.append(key)
-        errors.append(value)
-plt.plot(ranging, errors, marker='o')
-plt.xticks(ranging)
-plt.xlabel('Latent Space Dimension')
-plt.ylabel('Evaluation Loss')
-plt.title('tmax=50000, t=10000, xmax=1000, x=100, k=0.01, q=100')
+
+##### PLOT SOLUTION #####
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot_surface(xx, tt, F, antialiased=False, linewidth=0, cmap=cm.coolwarm)
+plt.xlabel('X')
+plt.ylabel('T')
 plt.show()
+
