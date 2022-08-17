94080b6e8eb06d03abd1dd98c79ec4d11ba60307
darin-momayezi <77948633+darin-momayezi@users.noreply.github.com>
2022-08-17 17:34:10 -0400

    Update 1D_PDE_Autoencoder.py

--- 1D_PDE_Autoencoder.py
+++ 1D_PDE_Autoencoder.py
@@ -5,35 +5,46 @@ from sklearn import preprocessing
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from OneDmeshgrid import x, dx, dt, k, q, a, b
-
-
-
-##### Parameters #####
-
-input_size = 2*x  # Number of spatial grid points from OneDcoarsegrid
-hidden1 = int(input_size/2)  # Size of hidden layer
-hidden2 = int(input_size/10)
-latent_dim = 4
-epochs = 200
-lr = 0.001  # Learning rate
-batch_size = 30
-iterations = 10  # Run autoencoder _ times
-train_split = 0.7  # Fraction of data for training
-shuffle = False
-    
-# Weight decay
-if a == 0 and b == 1:
-    weight_decay = 1e-5
-elif a == 1 and b == 0:
-    weight_decay = 0
-else: weight_decay = 0
+from OneDmeshgrid import x, t, dx, dt, k, q, a, b, length, use_Fst
 
 
 
 ##### Data #####
-
 df = pd.DataFrame(np.load('/Users/darinmomayezi/Downloads/NSE2D[71]/1Dmeshgrid.npy'))
+
+
+
+##### Parameters #####
+# Use high-dimensional input vectors
+if use_Fst:
+    input_size = int(length*x)  # Number of spatial grid points from OneDcoarsegrid
+if not use_Fst:
+    input_size = int(x)
+
+hidden1 = int(input_size/2)  # Size of hidden layer
+hidden2 = int(input_size/10)
+
+latent_dim = 4
+
+if a == 0 and b == 1:
+    epochs = 1000
+    lr = 0.001
+    weight_decay = 0
+elif a == 1 and b == 1:
+    epochs = 2000
+    lr = 0.001
+    weight_decay = 0
+elif a == 1 and b == 0:
+    epochs = 400
+    lr = 0.001
+    weight_decay = 0
+
+batch_size = 30
+iterations = 3  # Run autoencoder _ times
+train_split = 0.8  # Fraction of data for training
+shuffle = False
+
+
 
 train_data = TensorDataset(torch.Tensor(preprocessing.normalize(df[:int(train_split*df.shape[0])])))
 eval_data = TensorDataset(torch.Tensor(preprocessing.normalize(df[int(train_split*df.shape[0]):])))
@@ -50,20 +61,15 @@ def autoencoder():
         def __init__(self):
             super().__init__()
             
-            
             self.encoder = nn.Sequential(nn.Linear(input_size, hidden1, bias=True, dtype=torch.float32),
-                                         nn.ReLU(),
-                                         nn.Linear(hidden1, hidden2, bias=True, dtype=torch.float32),
-                                         nn.ReLU(),
-                                         nn.Linear(hidden2, latent_dim, bias=True, dtype=torch.float32))
+                                            nn.Sigmoid(),
+                                            nn.Linear(hidden1, latent_dim, bias=True, dtype=torch.float32))
+                
+            self.decoder = nn.Sequential(nn.Linear(latent_dim, hidden1, bias=True, dtype=torch.float32),
+                                            nn.Sigmoid(),
+                                            nn.Linear(hidden1, input_size, bias=True, dtype=torch.float32))
             
-            self.decoder = nn.Sequential(nn.Linear(latent_dim, hidden2, bias=True, dtype=torch.float32),
-                                         nn.ReLU(),
-                                         nn.Linear(hidden2, hidden1, bias=True, dtype=torch.float32),
-                                         nn.ReLU(),
-                                         nn.Linear(hidden1, input_size, bias=True, dtype=torch.float32))
-            
-            torch.nn.init.uniform_(self.encoder[0].weight.data, -0.001, 0.001)
+            torch.nn.init.uniform_(self.encoder[0].weight.data, -0.01, 0.01)
             # print("\nWeight after sampling from Uniform Distribution:\n")
             # print(self.encoder[0].weight)
  
@@ -82,8 +88,13 @@ def autoencoder():
     
     if a == 1 and b == 0:
         scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100],gamma=0.1)    
-    else:
-        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150],gamma=0.1)
+        
+    elif a == 0 and b == 1:
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=50)
+        
+    elif a == 1 and b == 1:
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=2000)
+
     
     
     train_loss = []
@@ -118,7 +129,7 @@ def autoencoder():
             losst.backward()
             optimizer.step()
             running_train_loss += losst.item()
-        scheduler.step()
+        scheduler.step(losst)
         
         for param_group in optimizer.param_groups:
             print('lr: ', param_group['lr'])
@@ -155,16 +166,15 @@ for iteration in range(iterations):  # Compare _ number of iterations
             
             latent_dims.append(key)
             losses.append(value[0])
-            all_losses.append(value[0])
         
                 
         # Enforce decreasing losses
         for idx, loss in enumerate(losses):  
             
-            if idx == 0 or idx == 1:  # Skip the first eval loss
+            if idx == 0:  # Skip the first eval loss
                 continue
             
-            elif loss > 6e-7:  # Get rid of losses that mess up graph
+            elif idx == 1 and loss > 4e-5:  # Get rid of losses that mess up graph
                 break
             
             elif (loss / losses[idx-1]) < 10 and idx < 3:  # Continue if this error smaller than the last
@@ -176,8 +186,10 @@ for iteration in range(iterations):  # Compare _ number of iterations
             
             elif idx == 3 and (loss / losses[idx-1]) < 10:  # Display results when the end is reached
                 plt.plot(latent_dims, losses, marker='o', label=f'{iteration+1}')
-                plt.legend(loc='upper right') 
+                plt.legend(loc='upper right')
                 decreasing = False  # Finished (eval losses done decreasing)
+                for loss in losses:
+                    all_losses.append(loss)
                 break
             
             else: 
@@ -197,17 +209,16 @@ else:
 
 print(all_losses)
 
-print(losses)
 plt.xticks(latent_dims)
 
-if a == 1 and b == 1:
-    plt.yticks([5e-7, 1e-7, 1e-8], ['5*10^-7', '10^-7', '10^-8'])
-elif a == 0 and b == 1:
-    plt.yticks([1e-6, 1e-7], ['10^-6', '10^-7'])
-elif a == 1 and b == 0:
-    plt.yticks([1e-6, 1e-7], ['10^-6', '10^-7'])
+largest_loss = 0
+for loss in all_losses:
+    if loss >= largest_loss:
+        largest_loss = loss       
+print(largest_loss)
+
     
 plt.xlabel('Latent Space Dimension')
 plt.ylabel('Evaluation Loss')
-plt.title(f'A={a}, B={b}, dt={dt}, dx={dx}, k={k*dx} * 1/dx, q={q*dx} * 1/dx')
+plt.title(f'A={a}, B={b}, x={x}, t={t}, dt={dt}, dx={round(dx)}, lr={lr}')
 plt.show()
