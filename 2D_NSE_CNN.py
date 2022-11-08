b6df04b949f265cf81b609015e230706ef1f90e0
Darin Momayezi <77948633+darin-momayezi@users.noreply.github.com>
2022-11-08 10:50:40 -0500

    Update 2D_NSE_CNN.py

--- 2D_NSE_CNN.py
+++ 2D_NSE_CNN.py
@@ -8,7 +8,7 @@ from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader
 from torch.nn import (Linear, ReLU, MSELoss, Sequential, Conv1d, Conv2d, Conv3d, Sigmoid, MaxUnpool2d, Dropout2d, AvgPool2d,
                       MaxPool2d, Module, Softmax, BatchNorm2d, Dropout, Flatten, Unflatten, ConvTranspose2d)
-from torch.optim import Adam, SGD
+from torch.optim import Adam
 
 # Creating custom data set from folder
 class NSEDataset(Dataset):
@@ -22,8 +22,7 @@ class NSEDataset(Dataset):
     
     def __getitem__(self, idx):
         matrix_path = os.path.join(self.root_dir, self.annotations.iloc[idx, 0])
-        print(matrix_path)
-        matrix = np.array(loadmat(matrix_path)['omega1_sub'])
+        matrix = np.array(loadmat(matrix_path)[omega_sub])
         if self.transform:
             matrix = self.transform(matrix)
         return torch.tensor(matrix, dtype=torch.float32)
@@ -33,58 +32,16 @@ def normalize(matrix):
     norm = np.linalg.norm(matrix[0], 1)
     matrix = matrix/norm
     return matrix
+           
 
-# Load training data
-training_data = NSEDataset(root_dir='/Users/darinmomayezi/Documents/Research/GrigorievLab/Autoencoder/2D_NSE/Vorticity_subdomain_train/',
-                annotation_file='/Users/darinmomayezi/Documents/Research/GrigorievLab/Autoencoder/2D_NSE/Vorticity_subdomain_train/omega_names_subdomain.csv',
-                transform=normalize)
-eval_data = NSEDataset(root_dir='/Users/darinmomayezi/Documents/Research/GrigorievLab/Autoencoder/2D_NSE/Vorticity_subdomain_test/',
-                annotation_file='/Users/darinmomayezi/Documents/Research/GrigorievLab/Autoencoder/2D_NSE/Vorticity_subdomain_test/omega_names_subdomain_test.csv',
-                transform=normalize)
-training_loader = DataLoader(training_data)
-eval_loader = DataLoader(eval_data)
-# print(next(iter(training_loader)).shape)  # size of input
-
-# # High dimensional input
-# idx = 0
-# previous = 0
-# new_training_data = []
-# count = 0
-# for input in training_loader:
-#     # print(input[0])  # 50x50
-#     count += 1
-#     if idx == 0:  # skip first matrix
-#         idx += 1
-#         previous = input[0]
-        
-#     elif (idx + 1) % 2 == 0:  # concatenate current matrix to previous
-#         row_idx = 0
-#         new_input = []
-#         for row in input[0]:
-#             new_row = torch.tensor(np.concatenate((row.numpy(), previous[row_idx].numpy())))
-#             new_input.append(new_row)
-#             row_idx += 1
-#         new_training_data.append(new_input)
-#         idx += 1
-        
-#     elif (idx + 1) % 2 == 1:
-#         previous = input[0]
-#         idx += 1
-
-# training_data = Dataset(new_training_data)
-# training_loader = DataLoader(training_data)
-# print(next(iter(training_loader)).shape)
-        
-
-            
-
-# Encoded dimension
+# Variables
 features = 10 # latent_dim
 kernel = 3
 stride = 1
 padding = 1
 
-save_weights = False
+save_weights = True
+save_features = False
 
 # Define the CNN
 def CNN():
@@ -97,36 +54,45 @@ def CNN():
             # n' = 512 + 2p - f + 1 = 510
             
             self.encode = Sequential(
-                Conv2d(1, 8, kernel, stride=stride, padding=padding),
+                Conv2d(1, int(features/4), kernel, stride=stride, padding=padding),
                 ReLU(),
-                Conv2d(8, 16, kernel, stride=stride, padding=padding),
-                AvgPool2d(2),
+                Conv2d(int(features/4), int(features/2), kernel, stride=stride, padding=padding),
+                ReLU(),
+                Conv2d(int(features/2), features, kernel, stride=stride, padding=padding),
+                Dropout2d(0.2))
                 
                 
                 # Linear Section
-                Flatten(start_dim=0),
-                Linear(int(16 * 25 * 25), 50),
-                ReLU(),
-                Linear(50, 25))
+                # Flatten(start_dim=0),
+                # Linear(int(10 * 64 * 50 * 50), 50),
+                # Sigmoid(),
+                # Linear(50, 50))
             
             
             # DECODER
             self.decode = Sequential(
                 # Linear Section
-                Linear(25, 50),
-                ReLU(),
-                Linear(50, int(16 * 50 * 50)),
-                Unflatten(dim=0, unflattened_size=(16, 50, 50)),
+                # Linear(50, 50),
+                # Sigmoid(),
+                # Linear(50, int(10 * 64 * 50 * 50)),
+                # Unflatten(dim=0, unflattened_size=(10, 64, 50, 50)),
             
                
-                Conv2d(16, 8, kernel, stride=stride, padding=padding),
+                Conv2d(features, int(features/2), kernel, stride=stride, padding=padding),
                 ReLU(),
-                Conv2d(8, 1, kernel, stride=stride, padding=padding))
+                Conv2d(int(features/2), int(features/4), kernel, stride=stride, padding=padding),
+                ReLU(),
+                Conv2d(int(features/4), 1, kernel, stride=stride, padding=padding))
 
             
         def forward(self, x):
             
             encoded = self.encode(x)
+            if save_features:
+                # Save features for dimensionality analysis
+                for i in range(features):
+                  matrix = pd.DataFrame(encoded[0][i].detach().numpy())
+                  matrix.to_csv(f'/Users/darinmomayezi/Documents/Research/GrigorievLab/Autoencoder/2D_NSE/Features/feature{i}.csv')
             decoded = self.decode(encoded)
             return decoded
 
@@ -135,7 +101,7 @@ def CNN():
         running_eval_loss = 0
         
         for input in eval_loader:
-            # input = torch.unsqueeze(input, 1)
+            input = torch.unsqueeze(input, 1)
             eval_prediction = model(input)
             losse = criterion(input, eval_prediction)
             running_eval_loss += losse.item()
@@ -148,26 +114,36 @@ def CNN():
     model = Net()
     criterion = MSELoss()
     optimizer = Adam(model.parameters(), lr=0.001)
-    # print(model)
+    if latent_dim > 8:
+        # Reuse weights for more consistent graphs
+        pretrained_dict = torch.load('/Users/darinmomayezi/Desktop/pretrained_weights2.pth')
+        model_dict = model.state_dict()
+        for k, v in pretrained_dict.items():
+            model_dict[k] = v
+        model_dict.update(model_dict)
+        print(len(pretrained_dict))
+        print(len(model.state_dict()))
+        # model_dict.update(pretrained_dict)
+        # model.load_state_dict(model_dict)
             
 
     # Training Loop
-    for e in range(10):
+    for e in range(200):
         model.train()
         running_loss = 0
         
         index = 0
         for input in training_loader:
-            input = torch.unsqueeze(input[0], 0)
+            input = torch.unsqueeze(input, 1)
             prediction = model(input)
             loss = criterion(input, prediction)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
-            if e >= 0 and index == 0:
-                print(input)
-                print(prediction)
+            # if e >= 0 and index == 0:
+            #     print(input[0][0])
+            #     print(prediction[0][0])
             index += 1
             
         epoch_loss = running_loss/len(training_loader)
@@ -181,45 +157,61 @@ def CNN():
         print('training loss: ', training_loss)
     evalDict[latent_dim] = eval_loss[-1]
     if save_weights:
-        torch.save(model.state_dict(), '/Users/darinmomayezi/Documents/Research/GrigorievLab/Autoencoder/2D_NSE/Vorticity_test/2D_NSE_CNN_weights.pth')
+        torch.save(model.state_dict(), '/Users/darinmomayezi/Desktop/pretrained_weights2.pth')
 
 
-    no_of_layers=0
-    conv_layers=[]
+# Run CNN and plot results for each subdomain in vorticity data
+for subdomain in range(1, 65):
     
-    model_children=list(model.children())
+    omega_sub = f'omega{subdomain}_sub'  # key to access data in dictionary when using loadmat
     
-    # for child in model_children:
-    #     if type(child)==Conv2d:
-    #         no_of_layers+=1
-    #         conv_layers.append(child)
-    #         print(child)
-    #     elif type(child)==Sequential:
-    #         for layer in child.children():
-    #             if type(layer)==Conv2d:
-    #                 no_of_layers+=1
-    #                 conv_layers.append(layer)
-    # print(conv_layers)
-
-# Run CNN and plot results
-training_loss = []  
-eval_loss = []
-latent_dims = [30]
-evalDict = {}
-for latent_dim in latent_dims:
-    features = latent_dim
-    if latent_dim not in evalDict:
-        evalDict[latent_dim] = 0 
+    # Load Data
+    training_data = NSEDataset(root_dir=f'/Users/darinmomayezi/Documents/Research/GrigorievLab/Autoencoder/2D_NSE/Vorticity_subdomain_data/Vorticity_subdomain{subdomain}_train',
+                annotation_file=f'/Users/darinmomayezi/Documents/Research/GrigorievLab/Autoencoder/2D_NSE/Vorticity_subdomain_data/Vorticity_subdomain{subdomain}_train/omega_names_subdomain.csv',
+                transform=normalize)
+    eval_data = NSEDataset(root_dir=f'/Users/darinmomayezi/Documents/Research/GrigorievLab/Autoencoder/2D_NSE/Vorticity_subdomain_data/Vorticity_subdomain{subdomain}_test',
+                annotation_file=f'/Users/darinmomayezi/Documents/Research/GrigorievLab/Autoencoder/2D_NSE/Vorticity_subdomain_data/Vorticity_subdomain{subdomain}_test/omega_names_subdomain_test.csv',
+                transform=normalize)
+    training_loader = DataLoader(training_data, batch_size=10)
+    eval_loader = DataLoader(eval_data, batch_size=10)
+    
+    training_loss = []  
+    eval_loss = []
+    evalDict = {}
+    
+    latent_dims = [8, 16, 24, 32, 48, 64]
+    for latent_dim in latent_dims:
+        features = latent_dim
         
-    CNN()  
+        if latent_dim not in evalDict:
+            evalDict[latent_dim] = 0 
+            
+        CNN()  
+        
+    losses = []
+    for key, value in evalDict.items():
+        losses.append(value)
+        
+    # Graph results
+    plt.figure(subdomain)  # creates a new plt canvas
+    plt.plot(latent_dims, losses, marker='o')
+    plt.xticks(latent_dims)
     
-latent_dims = []
-losses = []
+    # Label loss if at least 3e-5 smaller than previous
+    yticks = []
+    yticks_labels = []
+    for index, loss in enumerate(losses):
+        if index == 0:
+            yticks.append(losses[index])
+            yticks_labels.append(str(round(losses[index], 5)))
+        elif (losses[index-1] - losses[index]) > 3e-5:
+            yticks.append(losses[index])
+            yticks_labels.append(str(round(losses[index], 5)))
+    plt.yticks(yticks, yticks_labels)
     
-for key, value in evalDict.items():
+    plt.xlabel('Latent Features')
+    plt.ylabel('Evaluation loss')
+    plt.title('Convlutional Autoencoder - 2D NSE')
+    plt.savefig(f'/Users/darinmomayezi/Documents/Research/GrigorievLab/Autoencoder/2D_NSE/Vorticity_subdomain_data/Vorticity_subdomain{subdomain}_test/AE_2D_NSE_subdomain{subdomain}.png')
     
-    latent_dims.append(key)
-    losses.append(value)
-   
-plt.plot(latent_dims, losses, marker='o')
-# plt.show()
+
